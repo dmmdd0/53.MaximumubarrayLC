b9807d8244fffc810a5bd93c3b70408604efc16b
dimm <witty.sock3113@fastmail.com>
2022-09-10 12:22:40 +0200

    done

--- main.go
+++ main.go
@@ -4,21 +4,23 @@ import "fmt"
 
 func main() {
 
-	arr := []int{-2, 1, -3, 4, -1, 2, 1, -5, 4}
+	arr := []int{-1}
 	arr = []int{1}
+	arr = []int{-2, 1, -3, 4, -1, 2, 1, -5, 4}
 	arr = []int{5, 4, -1, 7, 8}
+	arr = []int{-1}
 	fmt.Println(maxSubArray(arr))
 
 }
 
 func maxSubArray(nums []int) int {
 
-	max := 0
+	max := nums[0]
 	summ := 0
 	for _, num := range nums {
 		summ += num
-		if summ < 0 {
-			summ = 0
+		if summ < num {
+			summ = num
 		}
 		if summ > max {
 			max = summ
